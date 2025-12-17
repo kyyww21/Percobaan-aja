61bc5be20e85efa841b75ce0761f7a273448bc50
Dikhi Wardani <fmvickey2@gmail.com>
2025-12-17 10:36:35 +0700

    git

---
+++ function.h
@@ -0,0 +1,16 @@
+#ifndef PERPUSTAKAAN_H
+#define PERPUSTAKAAN_H
+
+void penambahanBuku();
+void cariBuku();
+void daftarBuku();
+void peminjamanBuku();
+void pengembalianBuku();
+void bubbleSortBuku();
+void selesai();
+void subMenu1();
+void subMenu2();
+void menu();
+
+
+#endif
